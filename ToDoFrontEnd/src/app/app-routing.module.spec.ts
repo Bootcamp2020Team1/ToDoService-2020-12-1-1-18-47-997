a8fba354290f4fdebec17230a5e6c056bd368b95
Xiaoyu Lu <xlu14@slb.com>
2020-12-01 13:09:31 +0800

    feat: add router to redirect when navigating to edit/id and detail/id

--- ToDoFrontEnd/src/app/app-routing.module.spec.ts
+++ ToDoFrontEnd/src/app/app-routing.module.spec.ts
@@ -1,16 +1,26 @@
-import { Location } from "@angular/common";
-import { TestBed, fakeAsync, tick } from "@angular/core/testing";
-import { RouterTestingModule } from "@angular/router/testing";
-import { Router } from "@angular/router";
+import { AppRoutingModule, routes } from './app-routing.module';
+import { Location } from '@angular/common';
+import { TestBed, fakeAsync, tick } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
+import { UpdateTodoItemComponent } from './update-todo-item/update-todo-item.component';
+import { TodoitemDetailComponent } from './todoitem-detail/todoitem-detail.component';
+import { CreateTodoitemComponent } from './create-todoitem/create-todoitem.component';
+import { ListTodoitemComponent } from './list-todoitem/list-todoitem.component';
 
-describe("Router", () => {
+describe('Router', () => {
   let location: Location;
   let router: Router;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule.withRoutes([])],
-      declarations: []
+      imports: [RouterTestingModule.withRoutes(routes)],
+      declarations: [
+        UpdateTodoItemComponent,
+        TodoitemDetailComponent,
+        CreateTodoitemComponent,
+        ListTodoitemComponent,
+      ]
     });
 
     router = TestBed.get(Router);
@@ -19,7 +29,21 @@ describe("Router", () => {
     router.initialNavigation();
   });
 
-  it('test navigate', fakeAsync(() =>{
+  it('navigate to "" redirects to /', fakeAsync(() => {
+    router.navigate(['']);
+    tick(50);
+    expect(location.path()).toBe('/');
+  }));
 
-  }))
+  it('navigate to "edit/1" should redirects to /edit/1', fakeAsync(() =>{
+    router.navigate(['edit/1']);
+    tick(50);
+    expect(location.path()).toBe('/edit/1');
+  }));
+
+  it('navigate to "detail/1" should redirects to /detail/1', fakeAsync(() =>{
+    router.navigate(['detail/1']);
+    tick(50);
+    expect(location.path()).toBe('/detail/1');
+  }));
 });
