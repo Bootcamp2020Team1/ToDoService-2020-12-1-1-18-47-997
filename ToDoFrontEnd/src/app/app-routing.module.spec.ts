27b9d4d36812db8358d926a3a1e7a4029f4e5fd7
Jialong ZHANG <JZhang182@slb.com>
2020-12-01 11:55:42 +0800

    test: add test for routing for create/detail/edit

--- ToDoFrontEnd/src/app/app-routing.module.spec.ts
+++ ToDoFrontEnd/src/app/app-routing.module.spec.ts
@@ -34,4 +34,22 @@ describe("Router", () => {
     tick(50);
     expect(location.path()).toBe('/');
   }))
+
+  it('navigate to "edit/:id" redirects to /edit/${id}', fakeAsync(() => {
+    router.navigate(['edit/1']);
+    tick(50);
+    expect(location.path()).toBe('/edit/1');
+  }))
+
+  it('navigate to "create" redirects to /create', fakeAsync(() => {
+    router.navigate(['create']);
+    tick(50);
+    expect(location.path()).toBe('/create');
+  }))
+
+  it('navigate to "detail/:id" redirects to /detail/${id}', fakeAsync(() => {
+    router.navigate(['detail/1']);
+    tick(50);
+    expect(location.path()).toBe('/detail/1');
+  }))
 });
