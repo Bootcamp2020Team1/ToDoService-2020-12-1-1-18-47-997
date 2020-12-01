771feac8f36b172275c186e2b981c21514d05911
Shishi Zhang <SZhang42@slb.com>
2020-12-01 11:38:42 +0800

    feat: add router to update component

--- ToDoFrontEnd/src/app/app-routing.module.ts
+++ ToDoFrontEnd/src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: '', component: ListTodoitemComponent },
   { path: 'create', component: CreateTodoitemComponent },
-  { path: 'edit', component: UpdateTodoItemComponent },
+  { path: 'edit/:id', component: UpdateTodoItemComponent },
   { path: 'detail', component: TodoitemDetailComponent },
 ];
 @NgModule({
